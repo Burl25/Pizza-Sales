ea83c342c50e0e3be39bcff26e099b37ace1cd45
Ioana <burlaciuc.ioana@gmail.com>
2026-05-28 15:23:40 +0300

    Dd connection update

--- Pizza Project py/db_connection.py
+++ Pizza Project py/db_connection.py
@@ -3,7 +3,7 @@ import pypyodbc as odbc
 def get_connection():
     connection = odbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=DESKTOP-S2H35L5\\SQLEXPRESS;'
+        'SERVER=Local host\\SQLEXPRESS;'
         'DATABASE=Pizza Project;'
         'Trusted_Connection=yes;'
     )
